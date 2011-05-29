45033fd371b39b0107b131587e115041d5e5005a
troyand <troyanovsky@gmail.com>
2011-05-29 22:12:55 +0300

    stuff

--- src/hermitian_code.py
+++ src/hermitian_code.py
@@ -1,6 +1,17 @@
 import unittest
+import logging
+
+logger = logging.getLogger('hermitian_code')
+logger.setLevel(logging.CRITICAL)
+while len(logger.parent.handlers) != 1:
+    logger.parent.removeHandler(logger.parent.handlers[-1])
+
+
 
 class DecodingError(Exception):
+    pass
+
+class CodeConstructionError(Exception):
     pass
 
 class HermitianCode():
@@ -24,31 +35,41 @@ class HermitianCode():
         self.t = int((self.a-3*self.g+1)/2)
         self.L_D = self.L(self.a)
         self.L_A = self.L(self.t + self.g)
-        self.L_B = self.L(self.t + 3*self.g + 1)
         self.L_C = self.L(self.a - self.t - self.g)
         self.S = Matrix(self.L_A).transpose() * Matrix(self.L_C)
-        #print self.S
+        logger.info('Syndrom matrix:\n%s' % self.S)
 
         # init points: P set and Q
         points = self.C.rational_points()
         self.Q = points[1]
         self.P = [points[0]] + points[2:]
+        logger.info('Points in P-set\n%s' % self.P)
 
         self.H()
+        logger.info('Parity-check matrix:\n%s' % self.H())
         self.G()
+        logger.info('Generator matrix:\n%s' % self.G())
         self.n = len(self.P)
         self.k = self.G().nrows()
         self.d = self.a - 2*self.g + 2
         #cache for applying function to the P set
         self.f_point_cache = {}
+        logger.info('Finished constructing code %s' % self)
 
     def __repr__(self):
-        return 'AG [%d, %d, %d] code on Hermitian curve H_%d <%s> with D=%d*Q' % (self.n, self.k, self.d, self.m, self.C, self.a)
+        return 'AG [%d, %d, %d] code on Hermitian curve H_%d <%s> with D=%d*Q' % (
+                self.n,
+                self.k,
+                self.d,
+                self.m,
+                self.C,
+                self.a
+                )
 
     def L(self, a):
         '''L(a*Q)'''
         if a <= 2*self.g - 2:
-            raise Exception('''The degree of Q is not greater than 2*g - 2
+            raise CodeConstructionError('''The degree of Q is not greater than 2*g - 2
                 %d <= 2*%d - 2''' % (a, self.g))
         L_D_basis = []
         m = self.m
@@ -59,25 +80,27 @@ class HermitianCode():
                     #print 'x^%d y^%d / z^%d' % (i, j, i+j)
                     L_D_basis.append((x**i * y**j)/z**(i+j))
         if len(L_D_basis) != int(a + 1 - self.g):
-            raise Exception('''The number of functions found does not
+            raise CodeConstructionError('''The number of functions found does not
             satisfy the Riemann-Roch theorem
             Found: %d, needed %d''' % (len(L_D_basis), a + 1 - self.g))
         return L_D_basis
 
     def _map_functions_points(self, functions, points):
-        return Matrix(self.F, [[self._apply(f, point) for point in points] for f in functions])
+        return Matrix(
+                self.F,
+                [[self._apply(f, point) for point in points] for f in functions])
 
     def H(self):
         try:
             return self._H
         except AttributeError:
-            a_dual = len(self.P) - 2 + 2*self.g - self.a
-            if a_dual < self.a and a_dual > 2*self.g - 2:
-                print 'using dual'
-                self._G = self._map_functions_points(self.L(a_dual), self.P)
-                self._H = Matrix(self.F, self._G.transpose().kernel().basis())
-            else:
-                self._H = self._map_functions_points(self.L_D, self.P)
+            #a_dual = len(self.P) - 2 + 2*self.g - self.a
+            #if a_dual < self.a and a_dual > 2*self.g - 2:
+            #    print 'using dual'
+            #    self._G = self._map_functions_points(self.L(a_dual), self.P)
+            #    self._H = Matrix(self.F, self._G.transpose().kernel().basis())
+            #else:
+            self._H = self._map_functions_points(self.L_D, self.P)
             return self._H
 
     def G(self):
@@ -110,15 +133,18 @@ class HermitianCode():
                 new_row.append(self.multiply(v, f))
             new_rows.append(new_row)
         S = Matrix(self.C.base_ring(), new_rows)
-        #print S
+        logger.info('Syndrom matrix for vector %s:\n%s' % (v, S))
         try:
             theta = vector(self.L_A)*vector(S.kernel().basis()[0])
+            logger.info('Error locator: %s' % theta)
         except IndexError:
             raise DecodingError
         error_positions = []
+        logger.info('Error locator equals to zero at following points')
         for i, p in enumerate(self.P):
             if self._apply(theta, p) == 0:
                 error_positions.append(i)
+                logger.info('P_%d' % i)
         error_value_system = []
         for f in self.L_D:
             row = []
@@ -126,13 +152,21 @@ class HermitianCode():
                 row.append(self._apply(f, self.P[i]))
             row.append(self.multiply(v, f))
             error_value_system.append(row)
-        error_value_system = Matrix(self.C.base_ring(), error_value_system).echelon_form()
-        #print error_value_system
-        ##print 'Error position and value:'
+        error_value_system = Matrix(
+                self.F,
+                error_value_system
+                )
+        logger.info('Error value system:\n%s' % error_value_system)
+        error_value_system = error_value_system.echelon_form()
+        logger.info('Error position and value:')
         for i, pos in enumerate(error_positions):
-            ##print pos, error_value_system[i][-1]
+            logger.info('P_%d: %s' % (pos, error_value_system[i][-1]))
             v[pos] -= error_value_system[i][-1]
-        decode_g = Matrix(self.C.base_ring(), self.G().rows() + [v]).transpose().echelon_form()
+        logger.info('Recovered vector: %s' % v)
+        decode_g = Matrix(
+                self.C.base_ring(),
+                self.G().rows() + [v]
+                ).transpose().echelon_form()
         decoded_message = [decode_g[i][-1] for i in range(0, decode_g.ncols()-1)]
         return decoded_message
 
@@ -157,7 +191,7 @@ def test(AG):
         if message == decoded:
             print 'Decoded correctly'
         else:
-            raise Exception('Failed to decode correctly')
+            raise DecodingError('Failed to decode correctly')
         #if raw_input() == 'q':
         #    break
     print 'Cached %d results for applying function to P-set' % len(AG.f_point_cache)
